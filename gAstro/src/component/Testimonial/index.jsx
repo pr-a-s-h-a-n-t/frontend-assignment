fe34efb942b5c4966ac4586149fd61a1bbadff6b
Prashant Mishra <prashantmishra111@outlook.com>
2023-04-25 21:54:03 +0530

    slider fix and cleanup

--- gAstro/src/component/Testimonial/index.jsx
+++ gAstro/src/component/Testimonial/index.jsx
@@ -166,7 +166,6 @@ export default function UserTestimonial() {
               height: "20vh",
             }}
           />
-          <MenuNav />
         </HeroSlider>
       </Box>
     </section>
